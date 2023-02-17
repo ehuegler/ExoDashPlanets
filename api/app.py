5fa585e30b9bf89a3eaccd96a6605f6cac6fba69
Ethan Huegler <ethan@huegler.net>
2023-02-17 13:35:09 -0500

    can pull variables from .env file

--- api/app.py
+++ api/app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
+from dotenv import load_dotenv
+import os
+
 
 app = Flask(__name__)
 
+# grab environment variables for connecting to the database
+load_dotenv()
 
 @app.route('/')
 def home():
