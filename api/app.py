0dd83498396401a97058b55d1cab8b001f7043be
AtWillB <46000428+AtWillB@users.noreply.github.com>
2023-02-21 20:16:18 -0500

    resolving issue_4, updating .gitignore

--- api/app.py
+++ api/app.py
@@ -1,9 +1,23 @@
 from flask import Flask, render_template
 from dotenv import load_dotenv
+import psycopg2
+import json
 import os
 
 
 app = Flask(__name__)
+
+# establishing connection to database
+f = open("../.dbase_conn.json")
+conn = json.load(f); f.close();
+connection = psycopg2.connect(user=conn['User'],
+                                password=conn['Password'],
+                                host=conn['Host'],
+                                port=conn['Port'],
+                                database=conn['Database'])
+cursor = connection.cursor()
+
+            
 
 """
 There will be more code here when the database is setup
@@ -41,12 +55,98 @@ Here are the api routes that will actuall do stuff like querying the database
 They are not returning html pages, just data. Flask expects these functions
 to return dictionaries which it will latter convert to JSON objects
 """
-@app.route('/api/num_planets')
+################################################################
+## Statistics
+
+# Total number of verified exoplanets
+@app.route('/api/stat/num_planets')
 def num_planets():
-    # this could have a better name than just data
-    data = {}
+    stat = {}
     
-    # this is where you would querry the database
-    data[num_planets] = 1000
+    cursor.execute("""SELECT Count(*) FROM exo_raw""")
+    result = cursor.fetchone()[0]
+
+    stat['num_planets'] = result
     
-    return data
+    return stat
+
+# Total number of solar systems with verified exoplanets
+@app.route('/api/stat/num_systems')
+def num_systems():
+    stat = {}
+    
+    cursor.execute("""SELECT Count(DISTINCT hostname) FROM exo_raw""")
+    result = cursor.fetchone()[0]
+
+    stat['num_systems'] = result
+    
+    return stat
+
+# Average number of planets per system
+@app.route('/api/stat/avg_num_planets')
+def avg_num_planets():
+    stat = {}
+
+    cursor.execute("""With pl_per_host as (
+                        select hostname, count(pl_name) as pl_count
+                        from "ExoDashPlanets".exo_raw
+                        group by hostname
+                        order by pl_count desc
+                      )
+
+                      select round(avg(pl_count), 6)
+                      from pl_per_host;""")
+    result = float(cursor.fetchone()[0])
+    stat['avg_num_planets'] = result
+
+    return stat
+
+# Average mass of planets in Earth masse's reported as mass. 
+# About a third of the planets don't show up here because they are in mass*sin(inc) units, 
+# mass*sin(inc)/sin(inc), or as a mass to radius ratio
+@app.route('/api/stat/avg_mass_planet_e')
+def avg_mass_planet_e():
+    stat = {}
+
+    cursor.execute("""select avg(pl_bmasse)
+                      from exo_raw
+                      where pl_bmassprov = 'Mass'""")
+    result = cursor.fetchone()[0]
+
+    stat['avg_mass_planet_e'] = result
+
+    return stat
+
+
+################################################################
+## Visualizations
+
+# returns X column, being the list of unique discovery methods
+# Y being the list of the frequency each method was used to discover the verified exoplanets
+@app.route('/api/vis/discovery_methods_bar')
+def detection_methods_bar():
+    vis = {}
+
+    cursor.execute("""select DISTINCT discoverymethod, count(discoverymethod)
+                    from exo_raw
+                    group by discoverymethod""")
+    result = cursor.fetchall()
+    X = [row[0] for row in result]
+    Y = [row[1] for row in result]
+
+    vis['X'] = X
+    vis['Y'] = Y
+
+    return vis
+    
+
+
+
+
+
+
+
+
+
+
+
